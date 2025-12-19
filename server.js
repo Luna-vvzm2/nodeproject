6289b0279d92cf16dd77dc9191ae64588fb3d619
Luna-vvzm2 <luna72344929@yahoo.co.jp>
2025-12-19 23:36:30 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// server.js
+
+// 1. Expressモジュールを読み込む
+const express = require('express');
+// 2. Expressアプリケーションのインスタンスを作成
+const app = express();
+// 3. サーバーがリッスンするポート番号を定義
+const port = 3000; // 例: 3000番ポート
+/*
+// 4. ルートパス ('/') へのGETリクエストを処理するルーティングを設定
+app.get('/', (req, res) => {
+  // res.send() はクライアントに文字列を送信するメソッド
+  res.send('Hello, Express! This is your first game server.');
+});
+*/
+
+const path = require("path");
+
+app.get("/about", (req, res) => {
+  const filePath = path.join(__dirname, "pages", "about.html");
+  res.sendFile(filePath);
+});
+
+app.get("/help", (req, res) => {
+  const filePath = path.join(__dirname, "pages", "help.html");
+  res.sendFile(filePath);
+});
+
+app.use(express.static("public"));
+
+// 5. サーバーを起動し、指定したポートでリクエストを待ち受ける
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+  console.log('Ctrl+C でサーバーを停止できます。');
+});
